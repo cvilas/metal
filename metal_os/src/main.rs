e4b58fe27c01c08fcd3a10f3ea4bbc0fe484d0f5
Vilas Chitrakaran <chitrakaran@arrival.com>
2021-02-21 21:53:03 +0000

    blog post 3 - printing to vga buffer

--- metal_os/src/main.rs
+++ metal_os/src/main.rs
@@ -1,11 +1,13 @@
 #![no_std]  // Don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points`
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 /// This function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -14,12 +16,7 @@ static HELLO: &[u8] = b"Hello World!";
 // The entry point, since the linker looks for a function named `_start` by default
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for(i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
